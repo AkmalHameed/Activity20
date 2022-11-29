00985527eb49739b5c9f323f75d2de7ee5cd86d7
Akmal Hameed <akmalhameed143@gamil.com>
2022-11-29 10:05:52 -0500

    file pushed to github

--- 02_parameters.sh
+++ 02_parameters.sh
@@ -1,13 +1,1 @@
-O
-#!/bin/sh
-# This is a comment!
-echo "Hello      World"       # This is a comment, too!
-echo "Hello World"
-echo "Hello * World"
-echo Hello * World
-echo Hello      World
-echo "Hello" World
-echo Hello "     " World
-echo "Hello "*" World"
-echo `hello` world
-echo 'hello' world
+"Hello" "World"
